d462a4a09cbc9631147d266d5881f91f966c107b
JayveeTheBest <jayveecabije@gmail.com>
2025-06-16 16:33:53 +0800

    Preparing for PythonAnywhere

--- facultyClearance/facultyClearance/settings.py
+++ facultyClearance/facultyClearance/settings.py
@@ -80,12 +80,8 @@ DEBUG = config('DEBUG', cast=bool)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -125,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'clearance/static']
 
 # Default primary key field type
