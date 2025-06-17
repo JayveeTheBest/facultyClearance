632267b179ce3906a4e7de987f1ab641f71e731b
JayveeTheBest <jayveecabije@gmail.com>
2025-06-17 14:03:32 +0800

    Updating Local settings.py with PA

--- facultyClearance/facultyClearance/settings.py
+++ facultyClearance/facultyClearance/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xv+rooi%*-vn+m+6&n9s&c3khmwzgf1sjd(iokn%-f8!wc-an4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
