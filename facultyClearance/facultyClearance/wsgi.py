8e28f28f8ed5e9edb8491f2d1289ff9b19e6861a
JayveeTheBest <jayveecabije@gmail.com>
2025-06-17 15:34:41 +0800

    updated wsgi.py

--- facultyClearance/facultyClearance/wsgi.py
+++ facultyClearance/facultyClearance/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for facultyClearance project.
+import os, sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+project_home = '/home/jayveecabije/facultyClearance/facultyClearance'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'facultyClearance.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facultyClearance.settings')
-
 application = get_wsgi_application()
